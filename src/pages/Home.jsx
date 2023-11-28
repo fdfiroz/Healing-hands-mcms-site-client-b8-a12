6be03e1c4f884be061aebd1ec8e16a9a535610f5
fdfiroz <firozahamed0@gmail.com>
2023-11-28 16:38:14 +0600

    Add Newsletter and PopularCamps components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Slider from "../components/Home/Slider"
+import Newsletter from "../components/Newsletter/Newsletter"
+import PopularCamps from "../components/PopularCamps/PopularCamps"
 import Testimonials from "../components/Testimonials/Testimonials"
 
 const Home = () => {
   return (
     <>
     <Slider></Slider>
+    <PopularCamps></PopularCamps>
     <Testimonials/>
+    <Newsletter/>
     </>
   )
 }
